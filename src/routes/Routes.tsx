429ea07db83bca84b2c0f3c21bb6ba0336357090
enamulhimel <mhasan173013@bscse.uiu.ac.bd>
2025-07-01 15:36:53 +0600

    routes created

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import HomePage from "../pages/HomePage";
+import CartPage from "../pages/CartPage";
+import ProductPage from "../pages/ProductPage";
+
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/cart",
+        element: <CartPage />,
+      },
+      {
+        path: "/contact",
+        element: <ProductPage />,
+      },
+    ],
+  },
+]);
+
+export default routes;
